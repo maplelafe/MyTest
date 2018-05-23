8117e055718fc3be9a6822562fc98701aac8e269
maplelafe <fangfubin2014@163.com>
2018-05-23 15:56:45 +0800

    commit

---
+++ src/test/GetModelNameAndType.java
@@ -0,0 +1,123 @@
+package test;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Date;
+
+class GetModelNameAndType {
+
+	public static void testReflect(Object model) throws SecurityException, NoSuchMethodException,
+			IllegalArgumentException, IllegalAccessException, InvocationTargetException, InstantiationException {
+		// 获取实体类的所有属性，返回Field数组
+		Field[] field = model.getClass().getDeclaredFields();
+		// 获取属性的名字
+		String[] modelName = new String[field.length];
+		String[] modelType = new String[field.length];
+		for (int i = 0; i < field.length; i++) {
+			// 获取属性的名字
+			String name = field[i].getName();//aaz308
+			modelName[i] = name;
+			// 获取属性类型
+			String type = field[i].getGenericType().toString();
+			modelType[i] = type;
+
+			// 关键。。。可访问私有变量
+			field[i].setAccessible(true);			
+			// 给属性设置
+			if (type.equals("class java.lang.Double")) {
+				field[i].set(model, 0.0);
+			}
+			else if (type.equals("class java.lang.Integer")) {
+				field[i].set(model, 0);
+			}
+			else if (type.equals("class java.lang.Long")) {
+				field[i].set(model, 0L);
+			} else if(type.equals("class java.lang.String")) {
+				field[i].set(model, "0");
+			}
+			//field[i].set(model, field[i].getType().getConstructor(field[i].getType()).newInstance("kou"));
+
+
+			// 将属性的首字母大写
+			name = name.replaceFirst(name.substring(0, 1), name.substring(0, 1).toUpperCase());
+
+			if (type.equals("class java.lang.Long")) {
+				// 如果type是类类型，则前面包含"class "，后面跟类名
+				Method m = model.getClass().getMethod("get" + name);
+				// 调用getter方法获取属性值
+				Long value = (Long) m.invoke(model);
+				if (value != null) {
+
+					System.out.println("attribute value:" + value);
+				}
+
+			}
+			if (type.equals("class java.lang.String")) {
+				// 如果type是类类型，则前面包含"class "，后面跟类名
+				Method m = model.getClass().getMethod("get" + name);
+				// 调用getter方法获取属性值
+				String value = (String) m.invoke(model);
+				if (value != null) {
+
+					System.out.println("attribute value:" + value);
+				}
+
+			}
+			if (type.equals("class java.lang.Integer")) {
+				Method m = model.getClass().getMethod("get" + name);
+				Integer value = (Integer) m.invoke(model);
+				if (value != null) {
+					System.out.println("attribute value:" + value);
+				}
+			}
+			if (type.equals("class java.lang.Short")) {
+				Method m = model.getClass().getMethod("get" + name);
+				Short value = (Short) m.invoke(model);
+				if (value != null) {
+					System.out.println("attribute value:" + value);
+				}
+			}
+			if (type.equals("class java.lang.Double")) {
+				Method m = model.getClass().getMethod("get" + name);
+				Double value = (Double) m.invoke(model);
+				if (value != null) {
+					System.out.println("attribute value:" + value);
+				}
+			}
+			if (type.equals("class java.lang.Boolean")) {
+				Method m = model.getClass().getMethod("get" + name);
+				Boolean value = (Boolean) m.invoke(model);
+				if (value != null) {
+					System.out.println("attribute value:" + value);
+				}
+			}
+			if (type.equals("class java.util.Date")) {
+				Method m = model.getClass().getMethod("get" + name);
+				Date value = (Date) m.invoke(model);
+				if (value != null) {
+					System.out.println("attribute value:" + value.toLocaleString());
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			testReflect(new Kc25());
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/test/Kc25.java
@@ -0,0 +1,807 @@
+package test;
+
+import java.util.Date;
+
+public class Kc25 implements java.io.Serializable {
+
+	// Fields
+
+	private Long aaz308;
+	private Long aaz216;
+	private Long aae283;
+	private Long aaz218;
+	private Long aaz217;
+	private String aaa027;
+	private String aka130;
+	private String aka030;
+	private Long aaz107;
+	private Long aaz159;
+	private String aae140;
+	private String eae020;
+	private Long aac001;
+	private String akc021;
+	private String aaa154;
+	private Long aae030;
+	private Long aae031;
+	private Long aaz001;
+	private String aab019;
+	private String aab020;
+	private Long eke055;
+	private Long eke054;
+	private Date akc194;
+	private Long ake010;
+	private Double akc264;
+	private Double eke060;
+	private Double akc253;
+	private Double elc006;
+	private Double akb081;
+	private Double aka151;
+	private Double eke018;
+	private Double eke019;
+	private Double eke020;
+	private Double ake038;
+	private Double ake034;
+	private Double ake036;
+	private Double ake039;
+	private Double ake029;
+	private Double ake035;
+	private Double ake026;
+	private Double eke024;
+	private Double ake043;
+	private Double ake032;
+	private Double ale001;
+	private Double aje001;
+	private Double ame001;
+	private Double eke045;
+	private Double eke046;
+	private Double eke048;
+	private Double eke049;
+	private Double elc007;
+	private Double ekb065;
+	private Double ekb066;
+	private Double akb067;
+	private String ekz025;
+	private Long aaz272;
+	private Long aaz285;
+	private Long eke059;
+	private String aka077;
+	private Long eke068;
+	private String aae013;
+	private Long aaz002;
+	private Long prseno;
+	private Double ekb068;
+	private Double eke170;
+	private Double eke171;
+	private Double eke172;
+	private Double eke173;
+	private Double eke174;
+	private Double ake044;
+	private Double ake027;
+
+
+	// Constructors
+	
+	public Double getEke170() {
+		return eke170;
+	}
+
+	public Double getAke044() {
+		return ake044;
+	}
+
+	public void setAke044(Double ake044) {
+		this.ake044 = ake044;
+	}
+
+	public Double getAke027() {
+		return ake027;
+	}
+
+	public void setAke027(Double ake027) {
+		this.ake027 = ake027;
+	}
+
+	public void setEke170(Double eke170) {
+		this.eke170 = eke170;
+	}
+
+	public Double getEke171() {
+		return eke171;
+	}
+
+	public void setEke171(Double eke171) {
+		this.eke171 = eke171;
+	}
+
+	public Double getEke172() {
+		return eke172;
+	}
+
+	public void setEke172(Double eke172) {
+		this.eke172 = eke172;
+	}
+
+	public Double getEke173() {
+		return eke173;
+	}
+
+	public void setEke173(Double eke173) {
+		this.eke173 = eke173;
+	}
+
+	public Double getEke174() {
+		return eke174;
+	}
+
+	public void setEke174(Double eke174) {
+		this.eke174 = eke174;
+	}
+
+	/** default constructor */
+	public Kc25() {
+	}
+
+	/** minimal constructor */
+	public Kc25(Long aaz308, Long aaz216, Long aae283, Long aaz218,
+			Long aaz217, String aaa027, String aka130, String aka030,
+			Long aaz107, Long aaz159, String aae140, String eae020,
+			Long aac001, String akc021, Long aae030, Long aae031, Long eke054,
+			Date akc194, Long ake010, Double akc264, Double eke060,
+			Double akc253, Double akb081, Double aka151, Double eke018,
+			Double eke019, Double eke020, Double ake038, Double ake034,
+			Double ake036, Double ake039, Double ake029, Double ake035,
+			Double ake026, Double eke024, Double ake043, Double ake032,
+			Double ale001, Double aje001, Double ame001, Double eke045,
+			Double eke046, Double eke048, Double eke049, Double elc007,
+			Double ekb065, Double ekb066, Double akb067, String aka077,
+			Long prseno) {
+		this.aaz308 = aaz308;
+		this.aaz216 = aaz216;
+		this.aae283 = aae283;
+		this.aaz218 = aaz218;
+		this.aaz217 = aaz217;
+		this.aaa027 = aaa027;
+		this.aka130 = aka130;
+		this.aka030 = aka030;
+		this.aaz107 = aaz107;
+		this.aaz159 = aaz159;
+		this.aae140 = aae140;
+		this.eae020 = eae020;
+		this.aac001 = aac001;
+		this.akc021 = akc021;
+		this.aae030 = aae030;
+		this.aae031 = aae031;
+		this.eke054 = eke054;
+		this.akc194 = akc194;
+		this.ake010 = ake010;
+		this.akc264 = akc264;
+		this.eke060 = eke060;
+		this.akc253 = akc253;
+		this.akb081 = akb081;
+		this.aka151 = aka151;
+		this.eke018 = eke018;
+		this.eke019 = eke019;
+		this.eke020 = eke020;
+		this.ake038 = ake038;
+		this.ake034 = ake034;
+		this.ake036 = ake036;
+		this.ake039 = ake039;
+		this.ake029 = ake029;
+		this.ake035 = ake035;
+		this.ake026 = ake026;
+		this.eke024 = eke024;
+		this.ake043 = ake043;
+		this.ake032 = ake032;
+		this.ale001 = ale001;
+		this.aje001 = aje001;
+		this.ame001 = ame001;
+		this.eke045 = eke045;
+		this.eke046 = eke046;
+		this.eke048 = eke048;
+		this.eke049 = eke049;
+		this.elc007 = elc007;
+		this.ekb065 = ekb065;
+		this.ekb066 = ekb066;
+		this.akb067 = akb067;
+		this.aka077 = aka077;
+		this.prseno = prseno;
+	}
+
+	/** full constructor */
+	public Kc25(Long aaz308, Long aaz216, Long aae283, Long aaz218,
+			Long aaz217, String aaa027, String aka130, String aka030,
+			Long aaz107, Long aaz159, String aae140, String eae020,
+			Long aac001, String akc021, String aaa154, Long aae030,
+			Long aae031, Long aaz001, String aab019, String aab020,
+			Long eke055, Long eke054, Date akc194, Long ake010, Double akc264,
+			Double eke060, Double akc253, Double elc006, Double akb081,
+			Double aka151, Double eke018, Double eke019, Double eke020,
+			Double ake038, Double ake034, Double ake036, Double ake039,
+			Double ake029, Double ake035, Double ake026, Double eke024,
+			Double ake043, Double ake032, Double ale001, Double aje001,
+			Double ame001, Double eke045, Double eke046, Double eke048,
+			Double eke049, Double elc007, Double ekb065, Double ekb066,
+			Double akb067, String ekz025, Long aaz272, Long aaz285,
+			Long eke059, String aka077, Long eke068, String aae013,
+			Long aaz002, Long prseno, Double ekb068) {
+		this.aaz308 = aaz308;
+		this.aaz216 = aaz216;
+		this.aae283 = aae283;
+		this.aaz218 = aaz218;
+		this.aaz217 = aaz217;
+		this.aaa027 = aaa027;
+		this.aka130 = aka130;
+		this.aka030 = aka030;
+		this.aaz107 = aaz107;
+		this.aaz159 = aaz159;
+		this.aae140 = aae140;
+		this.eae020 = eae020;
+		this.aac001 = aac001;
+		this.akc021 = akc021;
+		this.aaa154 = aaa154;
+		this.aae030 = aae030;
+		this.aae031 = aae031;
+		this.aaz001 = aaz001;
+		this.aab019 = aab019;
+		this.aab020 = aab020;
+		this.eke055 = eke055;
+		this.eke054 = eke054;
+		this.akc194 = akc194;
+		this.ake010 = ake010;
+		this.akc264 = akc264;
+		this.eke060 = eke060;
+		this.akc253 = akc253;
+		this.elc006 = elc006;
+		this.akb081 = akb081;
+		this.aka151 = aka151;
+		this.eke018 = eke018;
+		this.eke019 = eke019;
+		this.eke020 = eke020;
+		this.ake038 = ake038;
+		this.ake034 = ake034;
+		this.ake036 = ake036;
+		this.ake039 = ake039;
+		this.ake029 = ake029;
+		this.ake035 = ake035;
+		this.ake026 = ake026;
+		this.eke024 = eke024;
+		this.ake043 = ake043;
+		this.ake032 = ake032;
+		this.ale001 = ale001;
+		this.aje001 = aje001;
+		this.ame001 = ame001;
+		this.eke045 = eke045;
+		this.eke046 = eke046;
+		this.eke048 = eke048;
+		this.eke049 = eke049;
+		this.elc007 = elc007;
+		this.ekb065 = ekb065;
+		this.ekb066 = ekb066;
+		this.akb067 = akb067;
+		this.ekz025 = ekz025;
+		this.aaz272 = aaz272;
+		this.aaz285 = aaz285;
+		this.eke059 = eke059;
+		this.aka077 = aka077;
+		this.eke068 = eke068;
+		this.aae013 = aae013;
+		this.aaz002 = aaz002;
+		this.prseno = prseno;
+		this.ekb068 = ekb068;
+	}
+
+	// Property accessors
+
+	public Long getAaz308() {
+		return this.aaz308;
+	}
+
+	public void setAaz308(Long aaz308) {
+		this.aaz308 = aaz308;
+	}
+
+	public Long getAaz216() {
+		return this.aaz216;
+	}
+
+	public void setAaz216(Long aaz216) {
+		this.aaz216 = aaz216;
+	}
+
+	public Long getAae283() {
+		return this.aae283;
+	}
+
+	public void setAae283(Long aae283) {
+		this.aae283 = aae283;
+	}
+
+	public Long getAaz218() {
+		return this.aaz218;
+	}
+
+	public void setAaz218(Long aaz218) {
+		this.aaz218 = aaz218;
+	}
+
+	public Long getAaz217() {
+		return this.aaz217;
+	}
+
+	public void setAaz217(Long aaz217) {
+		this.aaz217 = aaz217;
+	}
+
+	public String getAaa027() {
+		return this.aaa027;
+	}
+
+	public void setAaa027(String aaa027) {
+		this.aaa027 = aaa027;
+	}
+
+	public String getAka130() {
+		return this.aka130;
+	}
+
+	public void setAka130(String aka130) {
+		this.aka130 = aka130;
+	}
+
+	public String getAka030() {
+		return this.aka030;
+	}
+
+	public void setAka030(String aka030) {
+		this.aka030 = aka030;
+	}
+
+	public Long getAaz107() {
+		return this.aaz107;
+	}
+
+	public void setAaz107(Long aaz107) {
+		this.aaz107 = aaz107;
+	}
+
+	public Long getAaz159() {
+		return this.aaz159;
+	}
+
+	public void setAaz159(Long aaz159) {
+		this.aaz159 = aaz159;
+	}
+
+	public String getAae140() {
+		return this.aae140;
+	}
+
+	public void setAae140(String aae140) {
+		this.aae140 = aae140;
+	}
+
+	public String getEae020() {
+		return this.eae020;
+	}
+
+	public void setEae020(String eae020) {
+		this.eae020 = eae020;
+	}
+
+	public Long getAac001() {
+		return this.aac001;
+	}
+
+	public void setAac001(Long aac001) {
+		this.aac001 = aac001;
+	}
+
+	public String getAkc021() {
+		return this.akc021;
+	}
+
+	public void setAkc021(String akc021) {
+		this.akc021 = akc021;
+	}
+
+	public String getAaa154() {
+		return this.aaa154;
+	}
+
+	public void setAaa154(String aaa154) {
+		this.aaa154 = aaa154;
+	}
+
+	public Long getAae030() {
+		return this.aae030;
+	}
+
+	public void setAae030(Long aae030) {
+		this.aae030 = aae030;
+	}
+
+	public Long getAae031() {
+		return this.aae031;
+	}
+
+	public void setAae031(Long aae031) {
+		this.aae031 = aae031;
+	}
+
+	public Long getAaz001() {
+		return this.aaz001;
+	}
+
+	public void setAaz001(Long aaz001) {
+		this.aaz001 = aaz001;
+	}
+
+	public String getAab019() {
+		return this.aab019;
+	}
+
+	public void setAab019(String aab019) {
+		this.aab019 = aab019;
+	}
+
+	public String getAab020() {
+		return this.aab020;
+	}
+
+	public void setAab020(String aab020) {
+		this.aab020 = aab020;
+	}
+
+	public Long getEke055() {
+		return this.eke055;
+	}
+
+	public void setEke055(Long eke055) {
+		this.eke055 = eke055;
+	}
+
+	public Long getEke054() {
+		return this.eke054;
+	}
+
+	public void setEke054(Long eke054) {
+		this.eke054 = eke054;
+	}
+
+	public Date getAkc194() {
+		return this.akc194;
+	}
+
+	public void setAkc194(Date akc194) {
+		this.akc194 = akc194;
+	}
+
+	public Long getAke010() {
+		return this.ake010;
+	}
+
+	public void setAke010(Long ake010) {
+		this.ake010 = ake010;
+	}
+
+	public Double getAkc264() {
+		return this.akc264;
+	}
+
+	public void setAkc264(Double akc264) {
+		this.akc264 = akc264;
+	}
+
+	public Double getEke060() {
+		return this.eke060;
+	}
+
+	public void setEke060(Double eke060) {
+		this.eke060 = eke060;
+	}
+
+	public Double getAkc253() {
+		return this.akc253;
+	}
+
+	public void setAkc253(Double akc253) {
+		this.akc253 = akc253;
+	}
+
+	public Double getElc006() {
+		return this.elc006;
+	}
+
+	public void setElc006(Double elc006) {
+		this.elc006 = elc006;
+	}
+
+	public Double getAkb081() {
+		return this.akb081;
+	}
+
+	public void setAkb081(Double akb081) {
+		this.akb081 = akb081;
+	}
+
+	public Double getAka151() {
+		return this.aka151;
+	}
+
+	public void setAka151(Double aka151) {
+		this.aka151 = aka151;
+	}
+
+	public Double getEke018() {
+		return this.eke018;
+	}
+
+	public void setEke018(Double eke018) {
+		this.eke018 = eke018;
+	}
+
+	public Double getEke019() {
+		return this.eke019;
+	}
+
+	public void setEke019(Double eke019) {
+		this.eke019 = eke019;
+	}
+
+	public Double getEke020() {
+		return this.eke020;
+	}
+
+	public void setEke020(Double eke020) {
+		this.eke020 = eke020;
+	}
+
+	public Double getAke038() {
+		return this.ake038;
+	}
+
+	public void setAke038(Double ake038) {
+		this.ake038 = ake038;
+	}
+
+	public Double getAke034() {
+		return this.ake034;
+	}
+
+	public void setAke034(Double ake034) {
+		this.ake034 = ake034;
+	}
+
+	public Double getAke036() {
+		return this.ake036;
+	}
+
+	public void setAke036(Double ake036) {
+		this.ake036 = ake036;
+	}
+
+	public Double getAke039() {
+		return this.ake039;
+	}
+
+	public void setAke039(Double ake039) {
+		this.ake039 = ake039;
+	}
+
+	public Double getAke029() {
+		return this.ake029;
+	}
+
+	public void setAke029(Double ake029) {
+		this.ake029 = ake029;
+	}
+
+	public Double getAke035() {
+		return this.ake035;
+	}
+
+	public void setAke035(Double ake035) {
+		this.ake035 = ake035;
+	}
+
+	public Double getAke026() {
+		return this.ake026;
+	}
+
+	public void setAke026(Double ake026) {
+		this.ake026 = ake026;
+	}
+
+	public Double getEke024() {
+		return this.eke024;
+	}
+
+	public void setEke024(Double eke024) {
+		this.eke024 = eke024;
+	}
+
+	public Double getAke043() {
+		return this.ake043;
+	}
+
+	public void setAke043(Double ake043) {
+		this.ake043 = ake043;
+	}
+
+	public Double getAke032() {
+		return this.ake032;
+	}
+
+	public void setAke032(Double ake032) {
+		this.ake032 = ake032;
+	}
+
+	public Double getAle001() {
+		return this.ale001;
+	}
+
+	public void setAle001(Double ale001) {
+		this.ale001 = ale001;
+	}
+
+	public Double getAje001() {
+		return this.aje001;
+	}
+
+	public void setAje001(Double aje001) {
+		this.aje001 = aje001;
+	}
+
+	public Double getAme001() {
+		return this.ame001;
+	}
+
+	public void setAme001(Double ame001) {
+		this.ame001 = ame001;
+	}
+
+	public Double getEke045() {
+		return this.eke045;
+	}
+
+	public void setEke045(Double eke045) {
+		this.eke045 = eke045;
+	}
+
+	public Double getEke046() {
+		return this.eke046;
+	}
+
+	public void setEke046(Double eke046) {
+		this.eke046 = eke046;
+	}
+
+	public Double getEke048() {
+		return this.eke048;
+	}
+
+	public void setEke048(Double eke048) {
+		this.eke048 = eke048;
+	}
+
+	public Double getEke049() {
+		return this.eke049;
+	}
+
+	public void setEke049(Double eke049) {
+		this.eke049 = eke049;
+	}
+
+	public Double getElc007() {
+		return this.elc007;
+	}
+
+	public void setElc007(Double elc007) {
+		this.elc007 = elc007;
+	}
+
+	public Double getEkb065() {
+		return this.ekb065;
+	}
+
+	public void setEkb065(Double ekb065) {
+		this.ekb065 = ekb065;
+	}
+
+	public Double getEkb066() {
+		return this.ekb066;
+	}
+
+	public void setEkb066(Double ekb066) {
+		this.ekb066 = ekb066;
+	}
+
+	public Double getAkb067() {
+		return this.akb067;
+	}
+
+	public void setAkb067(Double akb067) {
+		this.akb067 = akb067;
+	}
+
+	public String getEkz025() {
+		return this.ekz025;
+	}
+
+	public void setEkz025(String ekz025) {
+		this.ekz025 = ekz025;
+	}
+
+	public Long getAaz272() {
+		return this.aaz272;
+	}
+
+	public void setAaz272(Long aaz272) {
+		this.aaz272 = aaz272;
+	}
+
+	public Long getAaz285() {
+		return this.aaz285;
+	}
+
+	public void setAaz285(Long aaz285) {
+		this.aaz285 = aaz285;
+	}
+
+	public Long getEke059() {
+		return this.eke059;
+	}
+
+	public void setEke059(Long eke059) {
+		this.eke059 = eke059;
+	}
+
+	public String getAka077() {
+		return this.aka077;
+	}
+
+	public void setAka077(String aka077) {
+		this.aka077 = aka077;
+	}
+
+	public Long getEke068() {
+		return this.eke068;
+	}
+
+	public void setEke068(Long eke068) {
+		this.eke068 = eke068;
+	}
+
+	public String getAae013() {
+		return this.aae013;
+	}
+
+	public void setAae013(String aae013) {
+		this.aae013 = aae013;
+	}
+
+	public Long getAaz002() {
+		return this.aaz002;
+	}
+
+	public void setAaz002(Long aaz002) {
+		this.aaz002 = aaz002;
+	}
+
+	public Long getPrseno() {
+		return this.prseno;
+	}
+
+	public void setPrseno(Long prseno) {
+		this.prseno = prseno;
+	}
+
+	public Double getEkb068() {
+		return this.ekb068;
+	}
+
+	public void setEkb068(Double ekb068) {
+		this.ekb068 = ekb068;
+	}
+
+}
